2ca43515526354ec96479ceaa3879b1516794dbe
Your Name <you@example.com>
2023-10-15 20:37:15 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: msadityavardhan18@gmail.com</p>
+      <p>Phone: +91 (9392584546)</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+// src/components/Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="header-content">
+        <div className="header-left">
+          <h1>Aditya Vardhan M S</h1>
+          <nav>
+            <ul>
+              
+            </ul>
+          </nav>
+        </div>
+        <div className="header-right">
+          <img src="photo.png" alt="Aditya" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
